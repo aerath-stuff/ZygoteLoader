2a2160fea2ed3bdbcb84fe48114718408731d9ad
frknkrc44 <krc440002@gmail.com>
2026-09-19 01:55:11 +0300

    Add KernelSU and APatch version indicators

--- runtime/src/main/assets/customize.d/20-enforce-magisk-version.sh
+++ runtime/src/main/assets/customize.d/20-enforce-magisk-version.sh
@@ -1,6 +1,14 @@
-# Enforce magisk version
-# Supported version: 24000+
+# Enforce Magisk version, skip for KernelSU or APatch
+# Supported Magisk version: 24000+
 
-[ "$MAGISK_VER_CODE" -lt 24000 ] && abort "! Unsupported magisk version: $MAGISK_VER_CODE (24000+ required)"
+if [ "$KSU" ]
+then
+    ui_print "- KernelSU version: $KSU_VER (ksud: $KSU_VER_CODE - kernel: $KSU_KERNEL_VER_CODE)"
+elif [ "$APATCH" ]
+then
+    ui_print "- APatch version: $APATCH_VER ($APATCH_VER_CODE)"
+else
+    [ "$MAGISK_VER_CODE" -lt 24000 ] && abort "! Unsupported magisk version: $MAGISK_VER_CODE (24000+ required)"
 
-ui_print "- Magisk version: $MAGISK_VER($MAGISK_VER_CODE)"
+    ui_print "- Magisk version: $MAGISK_VER ($MAGISK_VER_CODE)"
+fi
